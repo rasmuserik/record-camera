527957c87f6faedf1f59e1dbb3f006b9b9fa4317
Rasmus Erik Voel Jensen <github-rasmuserik@solsort.dk>
2019-04-24 07:59:22 +0200

    recording

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import Radio from "@material-ui/core/Radio"
 import FormControlLabel from "@material-ui/core/FormControlLabel"
 import Typography from "@material-ui/core/Typography"
 import Grid from "@material-ui/core/Grid"
+import RecordRTC from "recordrtc"
+import { promisify } from "util"
 
 const styles = theme => ({
   button: {
@@ -20,7 +22,6 @@ class App extends Component {
     videoDevice: "",
     audioDevice: "",
     stream: undefined,
-    streams: [],
     devices: []
   }
   constructor() {
@@ -43,10 +44,10 @@ class App extends Component {
     const stream = await navigator.mediaDevices.getUserMedia({
       audio: false,
       video: this.state.videoDevice
-        ? true
-        : {
-            deviceId: this.state.videoDevice
+        ? {
+            deviceId: { exact: this.state.videoDevice }
           }
+        : true
     })
     this.setState({ stream })
     const devices = await window.navigator.mediaDevices.enumerateDevices()
@@ -55,20 +56,12 @@ class App extends Component {
     video.srcObject = stream
     video.play()
   }
-  componentDidUpdate() {
-    const { streams } = this.state
-    for (const { id, stream } of streams) {
-      const video = document.getElementById(id)
-      console.log("didUpdate", video, stream)
-      video.srcObject = stream
-      video.play()
-    }
-  }
+  componentDidUpdate() {}
   render() {
     setTimeout(() => this.startCamera(), 0)
     return (
       <Grid container spacing={24}>
-        <Grid item xs={6}>
+        <Grid item xs={5}>
           <RadioGroup
             value={this.state.videoDevice}
             onChange={e => this.setState({ videoDevice: e.target.value })}
@@ -85,7 +78,7 @@ class App extends Component {
               ))}
           </RadioGroup>
         </Grid>
-        <Grid item xs={6}>
+        <Grid item xs={5}>
           <RadioGroup
             value={this.state.audioDevice}
             onChange={e => this.setState({ audioDevice: e.target.value })}
@@ -102,8 +95,41 @@ class App extends Component {
               ))}
           </RadioGroup>
         </Grid>
+        <Grid item xs={2}>
+          <a id="dllink" style={{ display: "none" }} />
+          {this.state.recording ? (
+            <Button
+              onClick={() => {
+                this.state.recorder.stopRecording(async o => {
+                  console.log(o)
+                  this.setState({ recording: false })
+                  let blob = await this.state.recorder.getBlob()
+                  console.log("here", blob)
+                  const url = window.URL.createObjectURL(blob)
+                  const a = document.getElementById("dllink")
+                  a.href = url
+                  //a.download = filename
+                  a.click()
+                  window.URL.revokeObjectURL(url)
+                })
+              }}
+            >
+              stop recording
+            </Button>
+          ) : (
+            <Button
+              onClick={() => {
+                const recorder = RecordRTC(this.state.stream, {})
+                recorder.startRecording()
+                this.setState({ recording: true, recorder })
+              }}
+            >
+              record
+            </Button>
+          )}
+        </Grid>
         <Grid item xs={12}>
-          <video id="cameraPreview" autoplay={true} />
+          <video id="cameraPreview" autoPlay={true} />
         </Grid>
       </Grid>
     )
