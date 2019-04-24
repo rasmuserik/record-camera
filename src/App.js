31f8e44a96db27f2f23f40095a9147a1888b9236
Rasmus Erik Voel Jensen <github-rasmuserik@solsort.dk>
2019-04-24 07:29:31 +0200

    selectable video device

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import RadioGroup from "@material-ui/core/RadioGroup"
 import Radio from "@material-ui/core/Radio"
 import FormControlLabel from "@material-ui/core/FormControlLabel"
 import Typography from "@material-ui/core/Typography"
+import Grid from "@material-ui/core/Grid"
 
 const styles = theme => ({
   button: {
@@ -17,25 +18,39 @@ const sleep = ms => new Promise(resolve => setTimeout(resolve, ms))
 class App extends Component {
   state = {
     videoDevice: "",
+    audioDevice: "",
+    stream: undefined,
     streams: [],
     devices: []
   }
   constructor() {
     super()
-    this.initCamera()
-  }
-  async initCamera() {
-    const devices = await window.navigator.mediaDevices.enumerateDevices()
-    this.setState({ devices })
-    console.log(devices)
+    this.startCamera()
   }
 
   async startCamera() {
+    console.log("here1")
     // setup camera
+    if (
+      this.state.prevVideo === this.state.videoDevice &&
+      this.state.prevAudio === this.state.audioDevice
+    ) {
+      return
+    }
+    this.state.prevVideo = this.state.videoDevice
+    this.state.prevAudio = this.state.audioDevice
+    console.log("here2")
     const stream = await navigator.mediaDevices.getUserMedia({
       audio: false,
-      video: true
+      video: this.state.videoDevice
+        ? true
+        : {
+            deviceId: this.state.videoDevice
+          }
     })
+    this.setState({ stream })
+    const devices = await window.navigator.mediaDevices.enumerateDevices()
+    this.setState({ devices })
     const video = document.getElementById("cameraPreview")
     video.srcObject = stream
     video.play()
@@ -50,22 +65,47 @@ class App extends Component {
     }
   }
   render() {
+    setTimeout(() => this.startCamera(), 0)
     return (
-      <div>
-        <RadioGroup
-          value={this.state.videoDevice}
-          onChange={e => this.setState({ videoDevice: e.target.value })}
-        >
-          {this.state.devices.map(({ deviceId, groupId, kind, label }) => (
-            <FormControlLabel
-              control={<Radio />}
-              key={deviceId}
-              value={deviceId}
-              label={deviceId + groupId + kind + label}
-            />
-          ))}
-        </RadioGroup>
-      </div>
+      <Grid container spacing={24}>
+        <Grid item xs={6}>
+          <RadioGroup
+            value={this.state.videoDevice}
+            onChange={e => this.setState({ videoDevice: e.target.value })}
+          >
+            {this.state.devices
+              .filter(o => o.kind === "videoinput")
+              .map(({ deviceId, groupId, kind, label }) => (
+                <FormControlLabel
+                  control={<Radio />}
+                  key={deviceId}
+                  value={deviceId}
+                  label={label}
+                />
+              ))}
+          </RadioGroup>
+        </Grid>
+        <Grid item xs={6}>
+          <RadioGroup
+            value={this.state.audioDevice}
+            onChange={e => this.setState({ audioDevice: e.target.value })}
+          >
+            {this.state.devices
+              .filter(o => o.kind === "audioinput")
+              .map(({ deviceId, groupId, kind, label }) => (
+                <FormControlLabel
+                  control={<Radio />}
+                  key={deviceId}
+                  value={deviceId}
+                  label={label}
+                />
+              ))}
+          </RadioGroup>
+        </Grid>
+        <Grid item xs={12}>
+          <video id="cameraPreview" autoplay={true} />
+        </Grid>
+      </Grid>
     )
   }
 }
